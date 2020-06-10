47d26ea1c9eae89eea4469c0b1289a69b87b7279
Tiago De Pina <tiagopina20014@gmail..com>
2020-06-10 04:06:54 +0100

    hustle

--- Server/server.js
+++ Server/server.js
@@ -1,11 +1,12 @@
 const express = require('express');
+const cors = require("cors")
 const bodyParser = require('body-parser');
 const app = express();
 const port = process.env.PORT || 3000;
 const router = require("./Routes/routes");
-const session = require('express-session');
+// const session = require('express-session');
 const helmet = require('helmet');
-const cors = require("cors")
+
 // const csrf = require('csurf');
 
 app.use(cors({
@@ -21,16 +22,16 @@ app.use(helmet.hsts({
     maxAge: 7776000000,
     includeSubDomains: true
 }));
-app.use(session({
-    secret: 'our super secret session secret',
-    saveUninitialized: true,
-    resave: true,
-    cookie: {
-        maxAge: 3600000,
-        secure: true,
-        httpOnly: true
-    }
-}));
+// app.use(session({
+//     secret: 'our super secret session secret',
+//     saveUninitialized: true,
+//     resave: true,
+//     cookie: {
+//         maxAge: 3600000,
+//         secure: true,
+//         httpOnly: true
+//     }
+// }));
 
 app.use(router);
 
