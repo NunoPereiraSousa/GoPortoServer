a16f5fe36ddfe68ba6e9fdcd694e317f5afea3a6
Nuno Sousa <9180579@esmad.ipp.pt>
2020-06-12 14:50:44 +0100

    Hustle

--- Server/Controller/identities.controller.js
+++ Server/Controller/identities.controller.js
@@ -59,7 +59,7 @@ function updateIdentity(req, res) {
     let lat = req.sanitize(req.body.lat);
     let lng = req.sanitize(req.body.lng);
     let image = req.sanitize(req.body.image);
-    con.query("UPDATE identity SET name = ?, information = ?, category_name = ?, lat = ?, lng = ?, image = ? WHERE id_identity = ?", [name, information, id_category, lat, lng, image, id_identity], function (err,
+    con.query("UPDATE identity SET name = ?, information = ?, category_name = ?, lat = ?, lng = ?, image = ? WHERE id_identity = ?", [name, information, category_name, lat, lng, image, id_identity], function (err,
         result) {
         if (!err) {
             res.status(200).send(result);
